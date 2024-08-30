7bebf6ff472a26e615c3eaef13f835447f85ddd1
Juan Alvarado <166556502+AlvaradoJ1@users.noreply.github.com>
2024-08-30 14:41:18 -0500

    par o impar
    Diseñe una solución básica con POO que permita resolver: si un número es par o impar y obtener su valor al cuadrado y al cubo.

---
+++ par o impar/par o impar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace par_o_impar
+{
+    class Program
+    {/* Diseñe una solución básica con POO que permita resolver: si un 
+        número es par o impar y obtener su valor al cuadrado y al cubo.
+        */
+        static void Main(string[] args)
+        {
+            int n1;
+            double cuadrado;
+            double cubo;
+
+            Console.WriteLine("Digite número ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (n1 % 2 == 1)
+            {
+                cuadrado = Math.Pow(n1, 2);
+                cubo = Math.Pow(n1, 3);
+                Console.WriteLine("El número es IMPAR");
+                Console.WriteLine("El cuadrado del número es: " +cuadrado);
+                Console.WriteLine("El cubo del número es: " +cubo);
+            }
+            else
+            {
+                cuadrado = Math.Pow(n1, 2);
+                cubo = Math.Pow(n1, 3);
+                Console.WriteLine("El número es PAR");
+                Console.WriteLine("El cuadrado del número es: " + cuadrado);
+                Console.WriteLine("El cubo del número es: " + cubo);
+
+            }
+        }
+    }
+}
